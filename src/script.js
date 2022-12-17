1ad11c8be65ac831166f9400aff825c447cbfdbc
Simon Baldin <simonmb92@gmail.com>
2022-12-17 19:26:57 +0600

    basic glass material

---
+++ src/script.js
@@ -0,0 +1,171 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as dat from "lil-gui";
+import { OBJ, OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
+
+//Stats
+(function () {
+  var script = document.createElement("script");
+  script.onload = function () {
+    var stats = new Stats();
+    document.body.appendChild(stats.dom);
+    requestAnimationFrame(function loop() {
+      stats.update();
+      requestAnimationFrame(loop);
+    });
+  };
+  script.src = "//mrdoob.github.io/stats.js/build/stats.min.js";
+  document.head.appendChild(script);
+})();
+
+//Loaders
+const objloder = new OBJLoader();
+const textureLoader = new THREE.TextureLoader();
+
+//Loading model and apply glass material
+const glassMaterial = new THREE.MeshPhysicalMaterial({
+  roughness: 0.1,
+  transmission: 1,
+  thickness: 1.5,
+});
+
+objloder.load("models/glass/glass_1.obj", (obj) => {
+  obj.traverse((child) => {
+    if (child instanceof THREE.Mesh) {
+      child.material = glassMaterial;
+    }
+  });
+  scene.add(obj);
+});
+
+const floorAndWallTexture = textureLoader.load("/textures/bc.png");
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI();
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Floor and wall
+ */
+const floorAndWallMaterial = new THREE.MeshStandardMaterial({
+  map: floorAndWallTexture,
+  metalness: 0,
+  roughness: 0.5,
+});
+
+const floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(10, 10),
+  floorAndWallMaterial
+);
+floor.receiveShadow = true;
+floor.rotation.x = -Math.PI * 0.5;
+
+const wall = new THREE.Mesh(
+  new THREE.PlaneGeometry(10, 10),
+  floorAndWallMaterial
+);
+wall.receiveShadow = true;
+wall.position.z = -5;
+wall.position.y = 5;
+scene.add(floor, wall);
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.set(1024, 1024);
+directionalLight.shadow.camera.far = 15;
+directionalLight.shadow.camera.left = -7;
+directionalLight.shadow.camera.top = 7;
+directionalLight.shadow.camera.right = 7;
+directionalLight.shadow.camera.bottom = -7;
+directionalLight.position.set(5, 5, 5);
+scene.add(directionalLight);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.set(2, 2, 2);
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.target.set(0, 0.75, 0);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.max(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+let previousTime = 0;
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - previousTime;
+  previousTime = elapsedTime;
+  // console.log(Math.round(1 / deltaTime));
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
